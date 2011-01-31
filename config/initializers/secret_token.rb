022d359f744406223fa830d10613cdb12d17b026
mylesmegyesi <myles.megyesi@gmail.com>
2011-01-31 17:29:48 -0600

    first commit rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geosight::Application.config.secret_token = '944e524ddb19dbbe701ecd3b05e7f702e4069b33a51675a273a7084d5a23f556db75a623f0ddefccf8b9f6c666841f79317b69b99336aa442bb71c8e97c56185'
