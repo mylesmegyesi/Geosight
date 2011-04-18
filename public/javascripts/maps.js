c58b9b2b8aa7190f0aab0b02f2919a99dc62c8ea
Joseph Leong <joseph.k.leong@gmail.com>
2011-04-18 13:50:13 -0500

    more refactoring

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,17 +1,10 @@
-
-/*
-// to preload images in infowindows
-function swapsource_and_dosomething(url,handler,element){
-  var img=new Image();
-  img.onload=function(){
-    element.src= url;
-    return handler(element);
-  }
-  img.src=url;
-}
-*/
-
 // adds photo markers to the map
+//   map - the map the marker will be added to
+//   lat - latitude
+//   lng - longitude
+//   cent - center
+// 	 contentString - the HTML to be displayed by the accompanying infowindows
+//   lastOpened - the last opened infowindow
 function add_photo_marker(map, lat, lng, cent, contentString, lastOpened) { 
   var latlng = new google.maps.LatLng(lat, lng);
 
@@ -37,7 +30,14 @@ function add_photo_marker(map, lat, lng, cent, contentString, lastOpened) {
 }
 
 // adds sight markers to the map
-function add_sight_marker(map, lat, lng, rad, title, contentString) { 
+//   map - the map the marker will be added to
+//   lat - latitude
+//   lng - longitude
+//   rad - the radius of the sight in meters
+//   title - the name of the sight
+// 	 contentString - the HTML to be displayed by the accompanying infowindows
+//   lastOpened - the last opened infowindow
+function add_sight_marker(map, lat, lng, rad, title, contentString, lastOpened) { 
     var latlng = new google.maps.LatLng(lat, lng);
     
     var marker = new google.maps.Marker({
@@ -56,4 +56,30 @@ function add_sight_marker(map, lat, lng, rad, title, contentString) {
         marker.infowindow.open(map,marker);
         lastOpened = marker;
     });  
+
+	var circle = new google.maps.Circle({
+        radius: rad,
+        center: new google.maps.LatLng(lat, lng),
+        map: map,
+        strokeWeight: 1
+		});
 }
+
+
+
+
+
+
+
+
+/*
+// to preload images in infowindows
+function swapsource_and_dosomething(url,handler,element){
+  var img=new Image();
+  img.onload=function(){
+    element.src= url;
+    return handler(element);
+  }
+  img.src=url;
+}
+*/
