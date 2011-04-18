af702305fa6e1801a978fd895f7a1d803eb0c7aa
Myles Megyesi <myles.megyesi@gmail.com>
2011-04-18 13:11:46 -0500

    Finished tag controller tests

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,6 @@ class Tag < ActiveRecord::Base
     has_and_belongs_to_many :photos
     has_and_belongs_to_many :sights
     validates_length_of :tag, :minimum => 1, :maximum => 20, :if => :tag?
-    
     private
     
     def rating?
--- test/functional/tags_controller_test.rb
+++ test/functional/tags_controller_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class TagsControllerTest < ActionController::TestCase
     setup do
         @tag = tags(:one)
-        @sight = sights(:one)      
+        @sight = sights(:one)   
+        @photo = photos(:one)
         @user = users(:one)
         UserSession.create(@user)
     end
@@ -24,11 +25,40 @@ class TagsControllerTest < ActionController::TestCase
         assert_redirected_to not_found_path
     end
     
-    test "should create tag" do
+    test "should create sight tag" do
         len = @sight.tags.length
         post :create, :sight_id => @sight.id, :tag => {:tag => "This is a tag"}
         sight = Sight.find_by_id(@sight.id)
         assert_equal (len+1), sight.tags.length
+    end
+    
+    test "should create photo tag" do
+        len = @photo.tags.length
+        post :create, :photo_id => @photo.id, :tag => {:tag => "This is a tag"}
+        photo = Photo.find_by_id(@photo.id)
+        assert_equal (len+1), photo.tags.length
+    end
+    
+    test "create - no tag paramater" do
+        post :create, :photo_id => @photo.id
+        assert_redirected_to photo_path(@photo)
+    end
+    
+    test "create - no tag" do
+        post :create, :photo_id => @photo.id, :tag => {}
+        assert_redirected_to photo_path(@photo)
+    end
+    
+    test "create - no parent" do
+        post :create, :tag => {}
+        assert_redirected_to not_found_path
+    end
+    
+    test "create - existing tag" do
+        tag = @photo.tags.first
+        assert_no_difference("Tag.count") do
+            post :create, :photo_id => @photo.id, :tag => {:tag => tag.tag}
+        end
     end
     
     test "should destroy photo_tag" do
@@ -36,6 +66,28 @@ class TagsControllerTest < ActionController::TestCase
         delete :destroy, :id => @tag.id, :sight_id => @sight.id
         sight = Sight.find_by_id(@sight.id)
         assert_equal (len-1), sight.tags.length
+    end
+    
+    test "should destroy tag" do
+        assert_difference("Tag.count", -1) do
+            @tag.photos.each do |photo|
+                delete :destroy, :id => @tag.id, :photo_id => photo.id
+            end
+            @tag.sights.each do |sight|
+                delete :destroy, :id => @tag.id, :sight_id => sight.id
+            end
+        end
+    end
+    
+    test "destroy - bad id" do
+        id = Tag.all.last.id + 1
+        delete :destroy, :id => id, :sight_id => @sight.id
+        assert_redirected_to not_found_path
+    end
+    
+    test "destroy - no parent" do
+        delete :destroy, :id => @tag.id
+        assert_redirected_to not_found_path
     end
         
 end
